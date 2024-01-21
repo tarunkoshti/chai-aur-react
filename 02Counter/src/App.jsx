78dc4f89b807eea97dc60ce33db336a6f37a4400
Tarun Koshti <tarunkoshti910@gmail.com>
2024-01-21 17:18:07 +0530

    Counter App

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+
+  let [counter, setcounter] = useState(0)
+  // let counter = 0;
+
+  const Add = () => {
+    // console.log(counter)
+    // counter++;
+    if (counter < 20) { setcounter(counter + 1); }
+  }
+
+  const Remove = () => {
+    // console.log(counter)
+    // counter++;
+    if (counter > 0) { setcounter(counter - 1); }
+  }
+
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h3>Counter value: {counter}</h3>
+    <button onClick={Add}>increase value: {counter}</button>
+    <br />
+    <button onClick={Remove}>decrease value: {counter}</button>
+    </>
+  )
+}
+
+export default App
