8b2d22cb83c68000231629cc778a8fba5fd1523e
Tarun Koshti <tarunkoshti910@gmail.com>
2024-01-21 14:06:14 +0530

    custom react and React.createElement()

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const MyApp = () => {
+    return (
+        <div>
+            <h1>Custom App | chai</h1>
+        </div>
+    )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com">Click me to vist google</a>
+)
+
+const reactElement = React.createElement(
+    'a',
+    {
+        href: 'https://google.com', target: '_blank'
+    },
+    'click me to visit google'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <App/>
 )
