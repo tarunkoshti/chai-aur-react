c03dd7a3a7fd67900181edfc3c0859e6fa47c0a5
Tarun Koshti <tarunkoshti910@gmail.com>
2024-01-22 22:56:42 +0530

    a react interview question on counter

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -9,7 +9,13 @@ const App = () => {
   const Add = () => {
     // console.log(counter)
     // counter++;
-    if (counter < 20) { setcounter(counter + 1); }
+    if (counter < 20) {
+      // setcounter(counter + 1)
+      // setcounter(counter + 1)
+      setcounter(prevCounter => prevCounter + 1)
+      setcounter(prevCounter => prevCounter + 1)
+
+    }
   }
 
   const Remove = () => {
@@ -20,11 +26,11 @@ const App = () => {
 
   return (
     <>
-    <h1>Chai aur react</h1>
-    <h3>Counter value: {counter}</h3>
-    <button onClick={Add}>increase value: {counter}</button>
-    <br />
-    <button onClick={Remove}>decrease value: {counter}</button>
+      <h1>Chai aur react</h1>
+      <h3>Counter value: {counter}</h3>
+      <button onClick={Add}>increase value: {counter}</button>
+      <br />
+      <button onClick={Remove}>decrease value: {counter}</button>
     </>
   )
 }
