f49dad6c1981c86bb79b9da733e6c778cd7dedab
Tarun Koshti <tarunkoshti910@gmail.com>
2024-02-26 01:50:59 +0530

    Select, PostCard, Login, Signup

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -2,6 +2,9 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
+import Button from "./Button"
+import Input  from "./Input";
 import LogoutBtn from "./Header/LogoutBtn";
 
-export { Header, Footer, Container, Logo, LogoutBtn }
+
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input,}
