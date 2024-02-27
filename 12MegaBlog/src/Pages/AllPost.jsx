3c63d1b35a8ffcf898fec60770b21fe5f5636661
Tarun Koshti <tarunkoshti910@gmail.com>
2024-02-27 15:38:37 +0530

    pages

---
+++ 12MegaBlog/src/Pages/AllPost.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import { Container, PostCard } from '../components'
+import appwriteService from '../appwrite/config'
+
+const AllPost = () => {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        appwriteService.getPosts([])
+            .then((posts) => {
+                if (posts) {
+                    setPosts(posts.documents)
+                }
+            })
+    }, [])
+
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {
+                        posts.map((post) => (
+                            <div key={post.$id} className='p-2 w-1/4'>
+                                <PostCard post={post} />
+                            </div>
+                        ))
+                    }
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default AllPost
--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,10 +3,30 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import Button from "./Button"
-import Input  from "./Input";
+import Input from "./Input";
 import LogoutBtn from "./Header/LogoutBtn";
 import Select from "./Select"
 import RTE from "./Editor"
+import Signup from './Signup'
+import Login from './Login'
+import Protected from './AuthLayout'
+import PostCard from './PostCard'
+import PostForm from './post-form/PostForm'
 
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select, RTE }
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    RTE,
+    Signup,
+    Login,
+    Protected,
+    PostCard,
+    PostForm
+}
